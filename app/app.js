e1aa374aa64c907bdd7a0eef350774cae0d4fc0e
geehaejeong <brainstation@BrainStations-MacBook-Air-8.local>
2016-02-10 13:32:56 -0500

    app.js

--- app/app.js
+++ app/app.js
@@ -14,9 +14,48 @@ angular
       	$stateProvider
       	.state('shop',{
       		url:'/',
-      		templateUrl:'',
+      		templateUrl:'site/partials/shop-main.html',
       		controller:'',
       	});
+
+
+
+    .state('admin',{
+      url:'/admin',
+      templateUrl:'site/partials/admin.html',
+      controller:'AdminCtrl as ctrl',
+      //TODO #2 Resolve Products before admin page load
+      resolve:{
+        products:function(productSrv){
+          return productSrv.getProducts();
+        }
+      }
+    })
+
+    .state('admin.dash',{
+      url:'/dashboard',
+      templateUrl:'site/partials/admin-dash.html'
+    })
+
+    .state('admin.add_product',{
+      url:'/add_product',
+      controller:'ProductCtrl as ctrl',
+      templateUrl:'site/partials/admin-add-product.html'
+    })
+
+    .state('admin.edit_product',{
+      url:'/edit_product/:productId',
+      controller:'ProductCtrl as ctrl',
+      templateUrl:'site/partials/admin-edit-product.html',
+    })
+
+    .state('auth',{
+      url:'/auth',
+      templateUrl:'site/partials/auth-main.html',
+      controller:'AuthCtrl as ctrl',
+    });
+
+
 
       	$httpProvider.interceptors.push(function(){
              return {
