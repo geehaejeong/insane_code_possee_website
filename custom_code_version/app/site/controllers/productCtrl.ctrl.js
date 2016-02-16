91dfcf685c3998852adec941f734196d90802583
geehaejeong <brainstation@BrainStations-MacBook-Air-8.local>
2016-02-16 11:21:16 -0500

    before pulling

--- custom_code_version/app/site/controllers/productCtrl.ctrl.js
+++ custom_code_version/app/site/controllers/productCtrl.ctrl.js
@@ -1,53 +1,69 @@
 (function() {
-        'use strict'
-        angular
-            .module('shopApp')
-            .controller('productCtrl', ProductCtrl);
+    'use strict'
+    angular
+        .module('shopApp')
+        .controller('productCtrl', ProductCtrl);
 
 
-        function ProductCtrl(api, productSrv,$state) {
-            var ctrl = this;
-            ctrl.api = api;
-            ctrl.productSrv = productSrv;
-            ctrl.$state=$state;
-            ctrl.addProduct = addProduct;
-            
-            ctrl.categories = [{
-            label: 'Pens',
-            value: 'pens'
-        }, {
-            label: 'Pencils',
-            value: 'pencils'
-        }, {
-            label: 'Planners',
-            value: 'planners'
-        }, {
-            label: 'Notebooks',
-            value: 'notesbooks'
-        } ];
+    function ProductCtrl(api, productSrv, $stateParams, $uibModal,products) {
+       
+        var ctrl = this;
+        ctrl.api = api;
+        ctrl.productSrv = productSrv;
+        ctrl.addProduct = addProduct;
+        ctrl.categories = ctrl.productSrv.categories;
+        ctrl.products = products;
+        ctrl.$uibModal = $uibModal;
+        ctrl.product = {};
+
+        ctrl.cart = [];
+        ctrl.animationsEnabled = true;
 
 
-            function addProduct() {
-                var product = {
-                    name: ctrl.name,
-                    image: ctrl.image,
-                    description: ctrl.description,
-                    category: ctrl.category,
-                    price: ctrl.price,
-                    quantity:ctrl.quantity,
-                    status: ctrl.status
-                }
+        ctrl.open = open;
 
-                ctrl.productSrv.addProduct(product);
+                ctrl.deleteProduct = deleteProduct;
+
+
+
+
+        function addProduct() {
+            var product = {
+                name: ctrl.name,
+                image: ctrl.image,
+                description: ctrl.description,
+                category: ctrl.category,
+                price: ctrl.price,
+                quantity: ctrl.quantity,
+                status: ctrl.status
             }
 
-
-      
+            ctrl.productSrv.addProduct(product);
         }
-<<<<<<< HEAD
+        function deleteProduct(id) {
+            ctrl.productSrv.deleteProduct(id);
+        }
 
-    })();
-=======
-    })();
+        function open() {   
+            console.log('modal');
+            var modalInstance = $uibModal.open({
+                animation: ctrl.animationsEnabled,
+                templateUrl: 'site/partials/cart.html',
+                controller: 'cartInstanceCtrl as ctrl',
+                size: 'sm',
 
->>>>>>> master
+
+            });
+
+        };
+
+
+        ctrl.toggleAnimation = function() {
+            ctrl.animationsEnabled = !ctrl.animationsEnabled;
+        };
+
+
+
+    }
+
+})();
